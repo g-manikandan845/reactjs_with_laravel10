f9be97556524a0b625f4ac1e3b25ad7c07b3aaca
manikandan.g <manikandan.g@synamen.com>
2023-03-26 20:58:15 +0530

    inertia installed successfully

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import { Head } from '@inertiajs/inertia-react';
+// import '../../css/app.css'
+
+export default function Test() {
+    return (
+        <h1 className='text-center font-bold '>Welcome Manikandan once upon a time</h1>
+    )
+} 
